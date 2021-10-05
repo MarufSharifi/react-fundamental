c0d1839c64c0e7b5ddba87d329909f22024c4c53
maruf <marufsharifi97103@gmail.com>
2021-10-05 07:47:15 +0430

    we are done with excersize 5

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,28 +1,30 @@
-// Styling
-// http://localhost:3000/isolated/exercise/05.js
-
 import * as React from 'react'
 import '../box-styles.css'
-
-// 💰 Use the className for the size and style (backgroundColor) for the color
-// 💰 each of the elements should also have the "box" className applied
-
-// 🐨 add a className prop to each of these and apply the correct class names
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightBlue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
+  )
+}
+
+function Box({size, style, ...rest}) {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...rest}
+    ></div>
   )
 }
 
