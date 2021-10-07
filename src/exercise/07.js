50429f0117b3cf81a2753d44c8f675df9c6fb983
maruf <marufsharifi97103@gmail.com>
2021-10-07 07:54:42 +0430

    succussfully done with rendrening array with react

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,6 +1,3 @@
-// Rendering Lists
-// http://localhost:3000/isolated/exercise/07.js
-
 import * as React from 'react'
 
 const allItems = [
@@ -29,8 +26,7 @@ function App() {
       </button>
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
-          // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item?.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
